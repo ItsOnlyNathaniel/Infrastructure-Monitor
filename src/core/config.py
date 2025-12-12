ec40be07870ca80f5792fa549d4dafe8c509e3fa
ItsOnlyNathaniel <2125418@swansea.ac.uk>
2025-12-12 20:33:08 +0000

    Mending docker inconsistencies and expanded schema for models. Models now consistently async

--- src/core/config.py
+++ src/core/config.py
@@ -4,7 +4,7 @@ class Settings(BaseSettings):
     #API Settings
     api_host: str = Field(default="0.0.0.0", alias="API_HOST")
     api_port: int = Field(default=8000, alias="API_PORT") 
-    log_level: str = Field(defaul="INFO", alias="LOG_LEVEL")
+    log_level: str = Field(default="INFO", alias="LOG_LEVEL")
 
     #Database Settings
     postgres_url: str = Field(alias="POSTGRES_URL")
@@ -17,7 +17,7 @@ class Settings(BaseSettings):
     #Localstack Settings
 
     #Monitoring Settings
-    health_check_intercval: int = 60
+    health_check_interval: int = 60
     remediation_timeout: int = 300
     max_retry_attempts: int = 3
 
